3d1e7df8ed9548a33c0de924cdfc4103e55af302
Daniel Binoy <dbinoy15@gmail.com>
2020-03-26 23:15:32 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const mongoDB = "mongodb://127.0.0.1:27017/blog_app"
-const port = 3000;
+const port = process.env.PORT || 3000;
 var express          = require("express"),
     app              = express(),
     methodOverride   = require("method-override")
